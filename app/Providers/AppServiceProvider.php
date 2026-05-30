8b35b9121809426b1ebb4ae276538603a7d3deed
baruitech <sourabarui@gmail.com>
2026-05-30 09:59:01 +0530

    Force HTTPS URLs in production; pass X-Forwarded-Proto to PHP-FPM

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // Force HTTPS URLs in production
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Share current tenant with all views if available
         View::composer('*', function ($view) {
             if (app()->has('current_tenant')) {
