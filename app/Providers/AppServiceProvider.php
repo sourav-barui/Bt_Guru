4107a3b5593f0ce41918fbb9f4d89839f6f228dd
baruitech <sourabarui@gmail.com>
2026-06-05 16:16:22 +0530

    Fix: AppServiceProvider - use Symfony Request constants

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,14 +21,14 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
 
             // Trust Cloudflare proxies for proper HTTPS detection
-            Request::setTrustedProxies(
+            \Illuminate\Http\Request::setTrustedProxies(
                 ['173.245.48.0/20', '103.21.244.0/22', '103.22.200.0/22', '103.31.4.0/22',
                  '141.101.64.0/18', '108.162.192.0/18', '190.93.240.0/20', '188.114.96.0/20',
                  '197.234.240.0/22', '198.41.128.0/17', '162.158.0.0/15', '104.16.0.0/13',
                  '104.24.0.0/14', '172.64.0.0/13', '131.0.72.0/22'],
-                Request::HEADER_X_FORWARDED_PROTO |
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT
+                SymfonyRequest::HEADER_X_FORWARDED_PROTO |
+                SymfonyRequest::HEADER_X_FORWARDED_HOST |
+                SymfonyRequest::HEADER_X_FORWARDED_PORT
             );
         }
 
